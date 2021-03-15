cd8530f528a8fdb9b7df12731d2bbe4bf46a0eff
Hyungyu Lim <flashizm@gmail.com>
2021-03-15 11:28:02 +0900

    list query sample

--- src/main/java/com/jellymoly/server/controller/MainController.java
+++ src/main/java/com/jellymoly/server/controller/MainController.java
@@ -2,6 +2,7 @@ package com.jellymoly.server.controller;
 
 import java.util.List;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,10 +14,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import egovframework.example.sample.service.EgovSampleService;
 import egovframework.example.sample.service.SampleDefaultVO;
 import egovframework.example.sample.service.SampleVO;
+import egovframework.rte.psl.dataaccess.util.EgovMap;
 
 @Controller
 public class MainController {
-	@Autowired
+	@Resource
 	private EgovSampleService service;
 
     @RequestMapping(value="/hello.do")
@@ -27,6 +29,29 @@ public class MainController {
     		SampleDefaultVO filter = new SampleDefaultVO();
 			List<SampleVO> list = (List<SampleVO>) service.selectSampleList(filter);
 	    	return String.valueOf( list.size() );
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    	return "error";
+    }
+
+    @RequestMapping(value="/list.do")
+    @ResponseBody
+    public String ListPage(HttpServletRequest request, ModelMap model)
+    {
+    	try {
+    		SampleDefaultVO filter = new SampleDefaultVO();
+			List<EgovMap> list = (List<EgovMap>) service.selectSampleList(filter);
+
+			StringBuffer sb = new StringBuffer();
+			for (EgovMap row : list) {
+				sb.append(row.toString());
+				sb.append("<br />");
+			}
+			
+	    	return String.valueOf( sb.toString() );
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
